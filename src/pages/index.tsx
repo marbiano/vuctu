92d96b931012c8d0462b2868623ac6305dc80ce8
marbiano <martin@Martins-MacBook-Pro.local>
2020-08-14 18:27:44 -0300

    Make use of SSG Next tooling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,37 +1,13 @@
-import { useState, useEffect } from 'react';
+import { GetStaticProps } from 'next';
 import Link from 'next/link';
 import { fetchAllProducts } from '../lib/api';
 import { Product } from '../lib/types';
 
 interface PageProps {
-  loading: boolean;
   products: Product[];
 }
 
-const IndexPage: React.FC = () => {
-  const [props, setProps] = useState<PageProps>({
-    loading: true,
-    products: [],
-  });
-
-  useEffect(() => {
-    async function fetchProducts() {
-      const newProducts = await fetchAllProducts();
-      setProps({
-        loading: false,
-        products: newProducts,
-      });
-    }
-
-    fetchProducts();
-  }, []);
-
-  const { loading, products } = props;
-
-  if (loading) {
-    return <div>Loading...</div>;
-  }
-
+const IndexPage: React.FC<PageProps> = ({ products }) => {
   if (products.length === 0) {
     return <div>There are no available products</div>;
   }
@@ -47,6 +23,15 @@ const IndexPage: React.FC = () => {
       ))}
     </ul>
   );
+};
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const products = await fetchAllProducts();
+  return {
+    props: {
+      products,
+    },
+  };
 };
 
 export default IndexPage;
--- src/pages/p/[slug].tsx
+++ src/pages/p/[slug].tsx
@@ -1,46 +1,41 @@
 import { useState, useEffect } from 'react';
-import { useRouter } from 'next/router';
+import { GetStaticProps, GetStaticPaths } from 'next';
 import DefaultErrorPage from 'next/error';
-import { fetchProductBySlug } from '../../lib/api';
+import { fetchAllProducts, fetchProductBySlug } from '../../lib/api';
 import { Product } from '../../lib/types';
 
 interface PageProps {
-  loading: boolean;
   product?: Product;
 }
 
-const ProductPage: React.FC = () => {
-  const [props, setProps] = useState<PageProps>({
-    loading: true,
-  });
-  const router = useRouter();
-  const { slug } = router.query;
-
-  useEffect(() => {
-    async function fetchProduct() {
-      const newProduct = await fetchProductBySlug(
-        Array.isArray(slug) ? slug[0] : slug,
-      );
-      setProps({
-        loading: false,
-        product: newProduct,
-      });
-    }
-
-    if (slug) fetchProduct();
-  }, [slug]);
-
-  const { loading, product } = props;
-
-  if (loading) {
-    return <div>Loading...</div>;
-  }
-
+const ProductPage: React.FC<PageProps> = ({ product }) => {
   if (!product) {
     return <DefaultErrorPage statusCode={404} />;
   }
 
   return <div>{product.fields.title}</div>;
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const { slug } = params;
+  const product = await fetchProductBySlug(
+    Array.isArray(slug) ? slug[0] : slug,
+  );
+  return {
+    props: {
+      product,
+    },
+  };
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const products = await fetchAllProducts();
+  return {
+    paths: products.map((product) => ({
+      params: { slug: product.fields.slug },
+    })),
+    fallback: false,
+  };
 };
 
 export default ProductPage;
