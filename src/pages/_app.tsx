844a27f628ea2a4c4b523f64948125f664b93d7f
marbiano <martin@Martins-MBP.fibertel.com.ar>
2020-08-18 03:35:29 -0300

    Add animation support

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,32 @@
 import { AppProps } from 'next/app';
+import { motion } from 'framer-motion';
 import '../styles/index.css';
 
-const MyApp: React.FC<AppProps> = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+const MyApp: React.FC<AppProps> = ({
+  Component,
+  pageProps,
+  router,
+}: AppProps) => {
+  return (
+    <motion.div
+      key={router.route}
+      transition={{ duration: 0.3, ease: 'easeOut' }}
+      initial="pageInitial"
+      animate="pageAnimate"
+      variants={{
+        pageInitial: {
+          opacity: 0,
+          y: 40,
+        },
+        pageAnimate: {
+          opacity: 1,
+          y: 0,
+        },
+      }}
+    >
+      <Component {...pageProps} />
+    </motion.div>
+  );
 };
 
 export default MyApp;
